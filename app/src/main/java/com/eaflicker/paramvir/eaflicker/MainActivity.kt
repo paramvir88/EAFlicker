833f94848a0632025508006d99d3d682ce047a17
Paramvir Singh <paramvir.singh88@gmail.com>
2019-01-03 12:37:53 +0530

    initial view sestup

--- app/src/main/java/com/eaflicker/paramvir/eaflicker/MainActivity.kt
+++ app/src/main/java/com/eaflicker/paramvir/eaflicker/MainActivity.kt
@@ -2,11 +2,73 @@ package com.eaflicker.paramvir.eaflicker
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
+    var photos: List<Photo> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var layoutManager = GridLayoutManager(this, 2)
+        rv.layoutManager = layoutManager
+        rv.adapter = RvAdapter(photos)
+
+
     }
+
+    class RvAdapter(var photosData: List<Photo>) : RecyclerView.Adapter<RvViewHolder>() {
+
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RvViewHolder {
+
+
+            var view = LayoutInflater.from(parent.context).inflate(R.layout.item_layout, parent, false)
+            var vh = RvViewHolder(view)
+            vh.bind()
+
+            return vh
+        }
+
+        override fun getItemCount(): Int {
+            return photosData.size
+        }
+
+        override fun onBindViewHolder(viewHolder: RvViewHolder, p1: Int) {
+            var photo = photosData[p1]
+            viewHolder.setData(photo)
+
+        }
+
+    }
+
+    class RvViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        var imageView : ImageView? = null
+        var titleTv: TextView? = null
+        fun bind(){
+            imageView = itemView.findViewById(R.id.iv_photo)
+            titleTv = itemView.findViewById(R.id.tv_title)
+        }
+
+        fun setData(photo: Photo){
+            titleTv?.text = photo.title
+        }
+
+
+    }
+
+
 }
+
+
---
+++ app/src/main/java/com/eaflicker/paramvir/eaflicker/Photo.kt
@@ -0,0 +1,27 @@
+package com.eaflicker.paramvir.eaflicker
+
+
+/**
+ * "id":"46529116962",
+"owner":"160158832@N05",
+"secret":"1b3c7b8820",
+"server":"7925",
+"farm":8,
+"title":"Star Chef: Cooking & Restaurant Game MOD Apk [Unlimited Money] v2.25 Android Download by 99Games",
+"ispublic":1,
+"isfriend":0,
+"isfamily":0,
+"url_o":"https:\/\/farm8.staticflickr.com\/7925\/46529116962_364f430e03_o.png",
+"height_o":"349",
+"width_o":"884"
+ */
+data class Photo(var id: String) {
+
+    var owner: String
+    var secret: String
+    var title: String
+    var url_o: String
+    var height: String
+    var width: String
+
+}
