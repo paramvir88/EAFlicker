abb456cae32e77fa0bd2c227990e95577e3d0ee0
Paramvir Singh <paramvir.singh88@gmail.com>
2019-01-03 11:17:13 +0530

    Initial commit

---
+++ app/src/main/java/com/eaflicker/paramvir/eaflicker/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.eaflicker.paramvir.eaflicker
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
