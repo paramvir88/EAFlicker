8e62fda38035ea0b0cb8e49402cb46f1dd5c2b11
Paramvir Singh <paramvir.singh88@gmail.com>
2019-01-04 10:39:16 +0530

    databse Room initial integration

--- app/src/main/java/com/eaflicker/paramvir/eaflicker/network/FlickrService.java
+++ app/src/main/java/com/eaflicker/paramvir/eaflicker/network/FlickrService.java
@@ -1,6 +1,10 @@
 package com.eaflicker.paramvir.eaflicker.network;
 
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
 import com.eaflicker.paramvir.eaflicker.Photo;
+import com.eaflicker.paramvir.eaflicker.database.FlickrDatabase;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -9,12 +13,20 @@ import java.util.List;
 
 public class FlickrService {
 
-    public void searchPhotos(String searchQuery, final GenericApiListener<List<Photo>, String> listener){
+    FlickrDatabase database;
+
+
+    void init(Context context) {
+         database = Room.databaseBuilder(context.getApplicationContext(), FlickrDatabase.class, "fl_databse").build();
+    }
+
+    public void searchPhotos(String searchQuery, final GenericApiListener<List<Photo>, String> listener) {
 
 
         ApiClient.getInterface().searchPhotos(searchQuery).enqueue(new Callback<FlickrResponse>() {
             @Override
             public void onResponse(Call<FlickrResponse> call, Response<FlickrResponse> response) {
+                database.photoDao().insertAll(response.body().photos.photo);
                 listener.onSucsess(response.body().photos.photo);
             }
 
